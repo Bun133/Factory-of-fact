365059d9a1ef32a8c61bbf40c978ae2ff0246e77
Bun133 <Bun133@users.noreply.github.com>
2019-10-31 17:51:30 +0900

    Update

--- source/src/main/Mainclass.java
+++ source/src/main/Mainclass.java
@@ -4,6 +4,8 @@ import javax.swing.JFrame;
 public class Mainclass {
 	public static int windowx=1920;
 	public static int windowy=1080;
+
+
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		JFrame mainflame=new JFrame("Title");
@@ -12,9 +14,14 @@ public class Mainclass {
 
 		mainflame.setTitle("Factory of fact");
 
-		//mainflame.setSize(windowx,windowy);
+		mainflame.setSize(windowx,windowy);
 
-		mainflame.setBounds(0, 0, windowx, windowy);
+		mainflame.setLocationRelativeTo(null);
+
+		mainflame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		mainflame.setExtendedState(mainflame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+		//mainflame.setBounds(0, 0, windowx, windowy);
 	}
 
 }
