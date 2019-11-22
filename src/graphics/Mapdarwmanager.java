6a339f6ec6c8f574fb19eccb568478771aac801f
Bun133 <akaaka1213820@gmail.com>
2019-11-22 21:07:34 +0900

    Delete gradle
    使えないし、eclipseのパッケージ一覧邪魔してくるので、

---
+++ src/graphics/Mapdarwmanager.java
@@ -0,0 +1,11 @@
+package graphics;
+
+import java.awt.Graphics;
+public class Mapdarwmanager extends JFrametools{
+	public Graphics g;
+	public long [] mapdata;
+	
+	public getGraphics() {
+		
+	}
+}
--- src/graphics/Showmap.java
+++ src/graphics/Showmap.java
@@ -6,64 +6,6 @@ public class Showmap {
 	}
 	public void draw(Graphics2D gra,int x,int y,String filepath) {
 
-<<<<<<< Updated upstream
 
-		File file=new File(filepath);
-		if (file.exists()==true) {
-
-
-				Loadobject loadmanager=new Loadobject();
-				objectdata=loadmanager.loadobject(filepath);
-				//ここに読み込みファイルからウィンドウ上に書きこみ。
-				//ファイルどう書くか決めないと。
-				Color c=new Color(0,0,0,0);//RGB+透明度(RGBA)
-
-				int add_x=0;
-				int add_y=0;
-				Objectroot objroot=new Objectroot();
-				int cnt=0;
-				while(cnt<objectdata.length){
-					if(objectdata[cnt]==null) {
-						System.out.println("data is null");
-						cnt++;
-						continue;
-					}
-					String a="";
-					String rgb="";
-					int int_a=101;
-					int int_r=256;
-					int int_g=256;
-					int int_b=256;
-					String[] data=objectdata[cnt].split("#");
-					a=data[0];
-					rgb=data[1];
-					int_a=Integer.parseInt(a);
-					int_r=Integer.parseInt(rgb.substring(5,6));
-					int_g=Integer.parseInt(rgb.substring(7,8));
-					int_g=Integer.parseInt(rgb.substring(9,10));
-					int_a=Integer.parseInt(a);
-					c=new Color(int_r,int_g,int_b,int_a);
-					add_x=objroot.getrootx(cnt,data.length);
-					add_y=objroot.getrooty(cnt,data.length);
-
-					gra.setColor(c);
-
-					gra.drawLine(x+add_x, y+add_y, x+add_x, y+add_x);
-
-
-
-
-					cnt++;
-				}
-
-				return gra;
-
-
-
-		}else {
-		System.out.println("File Not Found");
-		return null;
-=======
->>>>>>> Stashed changes
 		}
 }
